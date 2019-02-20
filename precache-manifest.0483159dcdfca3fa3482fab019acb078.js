ee8e56a338ff9cc0ac4dd41cdddf161f6ffa2de9
Vasiliy Zozulya <zazevang@gmail.com>
2019-02-20 21:39:09 +0300

    Updates

---
+++ precache-manifest.0483159dcdfca3fa3482fab019acb078.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "617a37f7d042342cef60",
+    "url": "/static/css/main.8d8c1d53.chunk.css"
+  },
+  {
+    "revision": "617a37f7d042342cef60",
+    "url": "/static/js/main.617a37f7.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4c9bd40ca84f228b97ed",
+    "url": "/static/js/2.4c9bd40c.chunk.js"
+  },
+  {
+    "revision": "665b409ff1ae972ebccda128b3f00d46",
+    "url": "/static/media/Roboto-Regular.665b409f.woff2"
+  },
+  {
+    "revision": "99433f7d92c3a5c1179fa7ebb1ba0a48",
+    "url": "/static/media/Roboto-Regular.99433f7d.woff"
+  },
+  {
+    "revision": "948404a07ea2d26a03e258b69e3319aa",
+    "url": "/index.html"
+  }
+];
